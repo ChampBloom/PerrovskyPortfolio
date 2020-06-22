a2d7b21859740928f4197d9e0ff4427d65260577
ChampBloom <abinadijorge@gmail.com>
2020-06-22 15:29:04 -0500

    JS Created
    Dark Mode Created

---
+++ PerrovskyPortfolio.js
@@ -0,0 +1,15 @@
+function toggleDarkMode() {
+    let darkButton = document.getElementById('body');
+    darkButton.classList.toggle('-dark');
+    console.log(darkButton);
+}
+
+
+
+function App(){
+    let darkMode = document.getElementById('button');
+
+    darkMode.addEventListener('click', toggleDarkMode);
+}
+
+document.addEventListener('DOMContentLoaded', App);
