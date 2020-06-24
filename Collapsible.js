50ce6fb2780ab28d3b506efb5e57aaee08d2643a
ChampBloom <abinadijorge@gmail.com>
2020-06-24 13:36:00 -0500

    Final version 1.0
    changed the colors and linked everything, made some changes to the project page

--- Collapsible.js
+++ Collapsible.js
@@ -1,3 +1,7 @@
+function toggleOpen() {
+    let disp = document.querySelector(".content");
+    disp.classList.toggle('-hide');
+}
 
 function toggleDarkMode() {
     let darkButton = document.getElementById('body');
@@ -5,13 +9,17 @@ function toggleDarkMode() {
     console.log(darkButton);
 }
 
-function App() {
+function App () {
+    let coll = document.getElementsByClassName("project_collapsible")
+
+    for (let i = 0; i < coll.length; i++){
+        coll[i].addEventListener('click', toggleOpen);
+    }
 
     let darkMode = document.getElementById('button');
 
     darkMode.addEventListener('click', toggleDarkMode);
 }
-
 
 document.addEventListener('DOMContentLoaded', App);
 
